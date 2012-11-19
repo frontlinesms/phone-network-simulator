d0b0afc9b5d66d877fd6f19142310be846b99ac0
jenkins <kubaikevin@yahoo.com>
2012-11-19 18:25:20 +0300

    Implemented save() in phone controller

--- grails-app/controllers/phonenetworksimulator/PhoneController.groovy
+++ grails-app/controllers/phonenetworksimulator/PhoneController.groovy
@@ -4,5 +4,14 @@ class PhoneController {
 	
 	def save() {
 		//TODO implement me
-	}
+		def phoneInstance = new Phone(params)
+        if (!phoneInstance.save(flush: true)) {
+            render(view: "create", model: [phoneInstance: phoneInstance])
+            return
+        }
+
+		flash.message = message(code: 'default.created.message', args: [message(code: 'messagingDevice.label', default: 'Phone'), phoneInstance.id])
+        redirect(action: "show", id: phoneInstance.id)
+    }
+
 }
