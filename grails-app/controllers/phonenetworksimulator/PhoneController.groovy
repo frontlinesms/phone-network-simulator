7748800bd719fc9a3f3c872eb4d9082132060320
jenkins <kubaikevin@yahoo.com>
2012-11-27 09:39:29 +0300

    Editing save function in PhoneController to handle Null Params

--- grails-app/controllers/phonenetworksimulator/PhoneController.groovy
+++ grails-app/controllers/phonenetworksimulator/PhoneController.groovy
@@ -3,15 +3,17 @@ package phonenetworksimulator
 class PhoneController {
 	
 	 
-def save(){
-   def phoneInstance = new Phone(params)
-
-     try {
-      phoneInstance.save(flush: true,failOnError:true)
-      redirect(controller:'message', action:'phone', params:[phoneNumber:params.phoneNumber])
-      } catch(Exception e){
-      redirect(controller:'message', action:'phone', params:[phoneNumber:params.phoneNumber])
-      println "The Param is Null"
-}
-}
+	def save(){
+	//if param is Null to remove Validationsave Exception
+		if(params.phoneNumber) {
+			def phoneInstance = new Phone(params)
+			phoneInstance.save(flush: true,failOnError:true)
+			redirect(controller:'message', action:'phone', params:[phoneNumber:params.phoneNumber])
+		} 
+		else {
+			flash.message = "Phone Number cannot be null"
+			redirect(controller:'message', action:'phone', params:[phoneNumber:params.phoneNumber])
+		}
+		
+	}
 }
