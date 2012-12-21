24b9c7d3c4ee00824d21b185fb56aedd330c8432
jenkins <kubaikevin@yahoo.com>
2012-12-21 12:13:12 +0300

    urlEncoder

--- grails-app/domain/phonenetworksimulator/Modem.groovy
+++ grails-app/domain/phonenetworksimulator/Modem.groovy
@@ -3,10 +3,14 @@ package phonenetworksimulator
 class Modem extends MessagingDevice{
 String url
 
-	def doSend(text, from) {
-		def request = url + "?text=${text}&from=${from}"
+	def doSend(text,from) {
+		def request = url + "?text=${urlEncode(text)}&from=${urlEncode(from)}"
 		(new URL(request)).getText()
+	
 	}
 
+	def urlEncode(text) {
+		return java.net.URLEncoder.encode(text)
+	}
    
 }
